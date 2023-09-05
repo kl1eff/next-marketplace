666f4bf8880123d2b13a415f79f13ceccfcefa50
vortex <you@example.com>
2023-09-05 17:58:30 +0300

    fix 4

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,21 +1,25 @@
-'use client'
+'use client';
 import { RootState } from '@/store';
 import styles from './styles.module.scss';
 import Link from 'next/link';
 import { FiShoppingCart } from 'react-icons/fi';
 import { useSelector } from 'react-redux';
 
-
 function Header() {
-  const itemsAmount = useSelector((state: RootState) => state.cart.items).length
+  const itemsAmount = useSelector((state: RootState) => state.cart.items).length;
   return (
     <header className={styles.header}>
-      <Link className={styles.logo} href='/'>ClickHead</Link>
+      <Link className={styles.logo} href="/">
+        ClickHead
+      </Link>
       <div>
-        <Link className={styles.button} href='/cart'><FiShoppingCart size={30} color='black'/></Link>
-        {(itemsAmount > 0) && <span>{itemsAmount}</span>}
+        <Link className={styles.button} href="/cart">
+          <div>
+            {itemsAmount > 0 && <span>{itemsAmount}</span>}
+            <FiShoppingCart size={30} color="black" />
+          </div>
+        </Link>
       </div>
-      
     </header>
   );
 }
